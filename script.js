08207b2a20701b8200e957aec8b4bc061dcd9c66
Ben Watkins <ben@e-neb.co.uk>
2013-09-29 13:03:47 +0100

    Update and rename main.js to script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 $(document).ready(function () {
   $("#check").click(function () {
     var pi_input = $("#pi-input").val(); 
-    for (i = 0; i < pi.length; i++) {
+    for (var i = 0; i < pi.length; i++) {
       if (pi_input[i] == pi[i]) {
         //more code
       }
-    }  
-  });
-});
+  };
+};
